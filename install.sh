bfc0f5b512b62ca2706053e5b414b9c135ec8ab0
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2026-06-10 12:52:30 -0300

    resolve install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ brew install figlet lolcat
 TITLE="HUEB CLI"
 figlet -f slant $TITLE | lolcat
 PYTHON_SCRIPT="https://raw.githubusercontent.com/rafaelhueb92/hueb-mac-cli/refs/heads/master/app.py"
-curl $PYTHON_SCRIPT
+curl $PYTHON_SCRIPT > app.py
 chmod +x app.py
 sudo mkdir -p /usr/local/bin/hueb && mv "$(pwd)/app.py" /usr/local/bin/hueb
 hueb -h
