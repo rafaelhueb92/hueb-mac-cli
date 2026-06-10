effb25e4d10fa9e7951a1768bfed89e10cbb1477
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2026-06-10 13:02:15 -0300

    set the script as hueb.py

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ TITLE="HUEB CLI"
 figlet -f slant $TITLE | lolcat
 PYTHON_SCRIPT="https://raw.githubusercontent.com/rafaelhueb92/hueb-mac-cli/refs/heads/master/app.py"
 curl -o app.py $PYTHON_SCRIPT 
-chmod +x app.py
-sudo mkdir -p /usr/local/bin/hueb && mv "$(pwd)/app.py" /usr/local/bin/hueb
+chmod +x hueb.py
+sudo mkdir -p /usr/local/bin/hueb && mv "$(pwd)/hueb.py" /usr/local/bin/hueb
 hueb -h
