8b89d5f547a3c4941e391562ac3ded192c069c87
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2026-06-10 14:41:08 -0300

    use ln and move to user dir the script

--- hueb.py
+++ hueb.py
@@ -289,4 +289,9 @@ def main() -> int:
         return 0
 
     mac_help = build_parser()
-    mac_help
+    mac_help.parse_args(["mac", "-h"])
+    return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
